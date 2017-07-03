952a42dccf9b7931aba069d75f0fa7fe70056ec5
arieibra kantor <adpit31@gmail.com>
2017-07-03 11:12:05 +0700

    tambahan path latihangolang

--- lab/helloworld/hello.go
+++ lab/helloworld/hello.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 
-	"github.com/adpit/lab/stringutil"
+	"github.com/adpit/latihangolang/lab/stringutil"
 	// 'workspace' C:\Users\adpit\Desktop\data\atom-x64-windows\clone\latihangolang\lab\stringutil (before)
 	// 'package' C:\Users\adpit\go\src\github.com\adpit\lab\stringutil (after)
 )
