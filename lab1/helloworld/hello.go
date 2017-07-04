95c843f85d9f5171d087d03f662281cc84329830
arieibra rumah <adpit31@gmail.com>
2017-07-05 05:37:51 +0700

    rename menjadi lab1, penjelasan materi library

--- lab1/helloworld/hello.go
+++ lab1/helloworld/hello.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 
-	"github.com/adpit/latihangolang/lab/stringutil"
+	"github.com/adpit/latihangolang/lab1/stringutil"
 	// 'workspace' C:\Users\adpit\Desktop\data\atom-x64-windows\clone\latihangolang\lab\stringutil (before)
 	// 'package' C:\Users\adpit\go\src\github.com\adpit\lab\stringutil (after)
 )
